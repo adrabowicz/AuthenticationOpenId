9e6b37f136e7b3c75e77772292190b2ac284965f
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-06-04 11:25:47 -0700

    added plumbing

---
+++ APIs/AegisApi/Controllers/TestController.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.Http;
+
+namespace AegisApi.Controllers
+{
+    public class TestController : ApiController
+    {
+    }
+}
---
+++ APIs/AegisApi/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Owin;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+
+namespace AegisApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // accept access tokens from identityserver and require a scope of 'MKP'
+            var tokenAuthenticationOptions = new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                ValidationMode = ValidationMode.ValidationEndpoint,
+
+                RequiredScopes = new[] { "MKP" }
+            };
+
+            // plug OWIN middleware component for token authentication into the pipeline
+            app.UseIdentityServerBearerTokenAuthentication(tokenAuthenticationOptions);
+
+            // configuration of HttpServer instance
+            var config = new HttpConfiguration();
+            // map the attribute-defined routes for the application
+            config.MapHttpAttributeRoutes();
+            // require authentication for all controllers
+            config.Filters.Add(new AuthorizeAttribute());
+
+            // plug OWIN middleware component for WebApi into the pipeline
+            app.UseWebApi(config);
+        }
+    }
+}
