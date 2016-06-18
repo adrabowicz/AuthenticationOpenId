3d9c976bf2bb8c14f2a6404d4e5f980ededc9b1b
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-06-18 11:27:21 -0700

    code refactoring

--- Clients/ConsoleClient/Program.cs
+++ Clients/ConsoleClient/Program.cs
@@ -9,12 +9,12 @@ namespace KpServerApp
         static void Main(string[] args)
         {
             // request the access token using the client credentials
-            var response = GetClientToken();
+            var response = GetClientToken("http://localhost:5000/connect/token");
             // call the API using the access token
             CallApi(response, "https://localhost:44351/test");
 
             // request an access token on behalf of a user
-            response = GetUserToken();
+            response = GetUserToken("http://localhost:5000/connect/token");
             // call the API using the access token
             CallApi(response, "https://localhost:44379/test");
         }
@@ -27,20 +27,20 @@ namespace KpServerApp
             Console.WriteLine(client.GetStringAsync(apiUrl).Result);
         }
 
-        static TokenResponse GetClientToken()
+        static TokenResponse GetClientToken(string identityServerConnectTokenUrl)
         {
             var client = new TokenClient(
-                "http://localhost:5000/connect/token",
+                identityServerConnectTokenUrl,
                 "Kp-Server-Machine",
                 "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
 
             return client.RequestClientCredentialsAsync("aegis.read").Result;
         }
 
-        static TokenResponse GetUserToken()
+        static TokenResponse GetUserToken(string identityServerConnectTokenUrl)
         {
             var client = new TokenClient(
-                "http://localhost:5000/connect/token",
+                identityServerConnectTokenUrl,
                 "Kp-Server-User",
                 "21B5F798-BE55-42BC-8AA8-0025B903DC3B");
 
