aaaf528f6bf16ca13e7d472d8a81b9948ac3b7bb
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-08-01 19:14:38 -0700

    added certificates and config

---
+++ WebHost/WebHost/Clients.cs
@@ -0,0 +1,40 @@
+﻿using CommonModule;
+using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHost
+{
+    class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientName = "KP MVC App - User",
+                    ClientId = "Kp-MvcApp-User",
+
+                    Flow = Flows.Implicit,
+                    Enabled = true,
+
+                    // allowed redirect Uris
+                    RedirectUris = new List<string>
+                    {
+                        Config.KpMvcAppBaseIp
+                    },
+
+                    // by default the OIDC middleware asks for two scopes: openid and profile
+                    AllowedScopes = new List<string>
+                    {
+                        "openid", "profile", "facility.read", "inventory.read", "inventory.readwrite"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ WebHost/WebHost/Users.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using System.Collections.Generic;
+using IdentityServer3.Core;
+using IdentityServer3.Core.Services.InMemory;
+
+namespace WebHost
+{
+    static class Users
+    {
+        public static List<InMemoryUser> Get()
+        {
+            // Subject is the unique identifier for that user that will be embedded into the access token.
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Username = "bob",
+                    Password = "secret",
+                    Subject = "BobDatabaseKey",
+
+                    Claims = new[]
+                    {
+                        new Claim(Constants.ClaimTypes.GivenName, "Bob"),
+                        new Claim(Constants.ClaimTypes.FamilyName, "Smith")
+                    }
+                },
+                new InMemoryUser
+                {
+                    Username = "alice",
+                    Password = "secret",
+                    Subject = "AliceDatabaseKey"
+                }
+            };
+        }
+    }
+}
