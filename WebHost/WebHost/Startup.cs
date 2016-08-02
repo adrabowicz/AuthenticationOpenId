c400db074947356e3f895de5ad3d05af06e06366
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-08-01 19:29:25 -0700

    added Startup.cs

---
+++ WebHost/WebHost/Startup.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer3.Core.Configuration;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static IdentityServer3.Core.Constants;
+
+namespace WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/identity", idsrvApp =>
+            {
+                idsrvApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Embedded IdentityServer",
+                    SigningCertificate = LoadCertificate(),
+
+                    Factory = new IdentityServerServiceFactory()
+                                .UseInMemoryUsers(Users.Get())
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(StandardScopes.All)
+                });
+            });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\identityServer\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+    }
+}
