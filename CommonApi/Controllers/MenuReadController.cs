7a8fdf7c5adb01f0c04ff04abf4fe73af0b7a640
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-01-28 12:06:04 -0800

    added package

--- CommonApi/Controllers/MenuReadController.cs
+++ CommonApi/Controllers/MenuReadController.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Security.Claims;
 using System.Web.Http;
+using Thinktecture.IdentityModel.WebApi;
 
 namespace CommonApi.Controllers
 {
+    [Route("test")]
+    [ScopeAuthorize("med.read", "med.readwrite")]
     class MenuReadController : ApiController
     {
         
