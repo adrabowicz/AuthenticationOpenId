72c6ab06a5267fab12cb313857c918a9690641b8
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-01-28 10:50:42 -0800

    added IDM client

--- Configuration/Clients.cs
+++ Configuration/Clients.cs
@@ -11,13 +11,32 @@ namespace Configuration
             {
                 new Client
                 {
+                    ClientName = "IDM",
+                    ClientId = "idm",
+                    Enabled = true,
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    Flow = Flows.ClientCredentials,
+
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("F621F470-9731-4A25-80EF-67A6F7C5F4B8".Sha256())
+                    },
+
+                    AllowedScopes = new List<string>
+                    {
+                        "cidm_identity.read"
+                    }
+                },
+                new Client
+                {
                     ClientName = "MA",
                     ClientId = "ma",
+                    Enabled = true,
                     AccessTokenType = AccessTokenType.Reference,
 
                     Flow = Flows.ResourceOwner,
-                    Enabled = true,
-
+                    
                     ClientSecrets = new List<Secret>
                     {
                         new Secret("21B5F798-BE55-42BC-8AA8-0025B903DC3B".Sha256())
