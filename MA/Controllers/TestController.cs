56c7cb4cda6e1b566b1d808b5ff1774d332bcde1
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-01-29 09:37:38 -0800

    added TestController

---
+++ MA/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace MA
+{
+    [Route("test")]
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var caller = User as ClaimsPrincipal;
+
+            return Json(new
+            {
+                message = "OK computer",
+                client = caller.FindFirst("client_id").Value
+            });
+        }
+    }
+}
