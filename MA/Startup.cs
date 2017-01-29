e62016f02241f69fdc931f2bcf84c01a1754cf9e
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-01-29 09:45:14 -0800

    added Startup file

---
+++ MA/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+
+namespace MA
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
