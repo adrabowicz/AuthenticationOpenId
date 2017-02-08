db7dea50690c909f0cbd0d196f87a6b728923064
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-02-07 19:43:54 -0800

    moved project

---
+++ APIs/CommonApi/Controllers/MenuReadController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Thinktecture.IdentityModel.WebApi;
+
+namespace CommonApi.Controllers
+{
+    [Route("test")]
+    [ScopeAuthorize("common_menu.read")]
+    class MenuReadController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
