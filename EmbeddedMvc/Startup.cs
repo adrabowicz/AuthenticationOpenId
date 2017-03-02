dfe3f6d9bd342e9bc09e4c1bcf75c458fbff7e0a
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-03-01 17:21:36 -0800

    claims transformation

--- EmbeddedMvc/Startup.cs
+++ EmbeddedMvc/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.OpenIdConnect;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Helpers;
+using System.Threading.Tasks;
 using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security;
 using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core;
 using Configuration;
@@ -55,7 +58,42 @@ namespace EmbeddedMvc
                 RedirectUri = "https://localhost:44319/",
                 ResponseType = "id_token",
 
-                SignInAsAuthenticationType = "Cookies"
+                SignInAsAuthenticationType = "Cookies",
+                UseTokenLifetime = false,
+
+                // notification that you can use to do claims transformation
+                // the resulting claims will be stored in the cookie
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = n =>
+                    {
+                        var id = n.AuthenticationTicket.Identity;
+
+                        // we want to keep first name, last name, subject and roles
+                        var givenName = id.FindFirst(Constants.ClaimTypes.GivenName);
+                        var familyName = id.FindFirst(Constants.ClaimTypes.FamilyName);
+                        var sub = id.FindFirst(Constants.ClaimTypes.Subject);
+                        var roles = id.FindAll(Constants.ClaimTypes.Role);
+
+                        // create new identity and set name and role claim type
+                        var newIdentity = new ClaimsIdentity(
+                            id.AuthenticationType,
+                            Constants.ClaimTypes.GivenName,
+                            Constants.ClaimTypes.Role);
+
+                        newIdentity.AddClaim(givenName);
+                        newIdentity.AddClaim(familyName);
+                        newIdentity.AddClaim(sub);
+                        newIdentity.AddClaims(roles);
+
+                        // add some other app specific claim
+                        newIdentity.AddClaim(new Claim("app_specific", "some data"));
+
+                        n.AuthenticationTicket = new AuthenticationTicket(newIdentity, n.AuthenticationTicket.Properties);
+
+                        return Task.FromResult(0);
+                    }
+                }
             });
         }
 
