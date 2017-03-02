96fef8615b4e0d14ba26840b27c76d9cc50a9503
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-03-01 17:13:47 -0800

    config

--- EmbeddedMvc/Startup.cs
+++ EmbeddedMvc/Startup.cs
@@ -2,9 +2,12 @@
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Web.Helpers;
 using Owin;
 using IdentityServer3.Core.Configuration;
-using IdentityServer3.Core.Models;
+using IdentityServer3.Core;
 using Configuration;
 
 namespace EmbeddedMvc
@@ -13,6 +16,12 @@ namespace EmbeddedMvc
     {
         public void Configuration(IAppBuilder app)
         {
+            // turn off mapping of claims to .NET ClaimTypes
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            // configuration for anti-CSRF protection
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = Constants.ClaimTypes.Subject;
+
             // if the request path starts with "/identity", execute the app configured via configuration parameter 
             // instead of continuing to the next component in the pipeline
             app.Map("/identity", idsrvApp =>
