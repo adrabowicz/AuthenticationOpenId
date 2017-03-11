ea78291bc03eebf320aacad712c03a59f0774335
bambus.ca@gmail.com <bambus.ca@gmail.com>
2017-03-11 09:37:10 -0800

    updated config

--- APIs/CommonApi/Startup.cs
+++ APIs/CommonApi/Startup.cs
@@ -9,17 +9,17 @@ namespace CommonApi
     {
         public void Configuration(IAppBuilder app)
         {
-            //// accept access tokens from identityserver and require a scope of 'MKP'
-            //var tokenAuthenticationOptions = new IdentityServerBearerTokenAuthenticationOptions
-            //{
-            //    Authority = Config.IdentityServerBaseIp,
-            //    ValidationMode = ValidationMode.ValidationEndpoint,
+            // accept access tokens from identityserver and require a scope of 'MKP'
+            var tokenAuthenticationOptions = new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = Config.IdentityServerBaseIp,
+                ValidationMode = ValidationMode.ValidationEndpoint,
 
-            //    RequiredScopes = new[] { "common_menu" }
-            //};
+                RequiredScopes = new[] { "common_menu" }
+            };
 
-            //// plug OWIN middleware component for token authentication into the pipeline
-            //app.UseIdentityServerBearerTokenAuthentication(tokenAuthenticationOptions);
+            // plug OWIN middleware component for token authentication into the pipeline
+            app.UseIdentityServerBearerTokenAuthentication(tokenAuthenticationOptions);
 
             // configuration of HttpServer instance
             var config = new HttpConfiguration();
