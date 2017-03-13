64e9c011ad9414296174c04b82ff3f5f1142872e
Drabowicz, Albert <adrabowicz@hotmail.com>
2017-03-13 07:39:13 -0700

    updated config

--- Configuration/Config.cs
+++ Configuration/Config.cs
@@ -3,19 +3,19 @@ namespace Configuration
 {
     public static class Config
     {
-        public const string Protocol = "http://";
-
-        public static string IdentityServerBaseIp = Protocol + "localhost:5000";
+        public static string IdentityServerBaseIp = "http://localhost:5000";
         public static string IdentityServerIdentityIp = IdentityServerBaseIp + "/identity";
         public static string IdentityServerConnectTokenIp = IdentityServerBaseIp + "/connect/token";
 
-        public static string CidmApiBaseIp = Protocol + "localhost:42001";
+        public static string CidmApiBaseIp = "http://localhost:42001";
         public static string CidmApiTestIp = CidmApiBaseIp + "/test";
 
-        public static string CommonApiBaseIp = Protocol + "localhost:42002";
+        public static string CommonApiBaseIp = "http://localhost:42002";
         public static string CommonApiTestIp = CommonApiBaseIp + "/test";
 
-        public static string KpApiBaseIp = Protocol + "localhost:42003";
+        public static string KpApiBaseIp = "http://localhost:42003";
         public static string KpApiTestIp = KpApiBaseIp + "/test";
+
+        public static string MvcAppBaseIp = "http://localhost:42020/";
     }
 }
