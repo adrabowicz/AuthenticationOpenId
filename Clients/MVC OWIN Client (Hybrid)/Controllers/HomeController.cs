d1406f0cd386b58d932914e217760a1583fa435d
Drabowicz, Albert <adrabowicz@hotmail.com>
2017-03-21 07:39:08 -0700

    modified views

--- Clients/MVC OWIN Client (Hybrid)/Controllers/HomeController.cs
+++ Clients/MVC OWIN Client (Hybrid)/Controllers/HomeController.cs
@@ -45,21 +45,6 @@ namespace MVC_OWIN_Client.Controllers
             return View();
         }
 
-        [Authorize]
-        public async Task<ActionResult> CallApi()
-        {
-            var token = (User as ClaimsPrincipal).FindFirst("access_token").Value;
-
-            var client = new HttpClient();
-            client.SetBearerToken(token);
-
-            var result = await client.GetStringAsync("Constants.AspNetWebApiSampleApi" + "identity");
-            ViewBag.Json = JArray.Parse(result.ToString());
-
-            return View();
-        }
-
-
         public ActionResult Signout()
         {
             // also possible to pass post logout redirect url via properties
