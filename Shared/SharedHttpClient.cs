710a80f684ab8ca19f194f62ce6e81191cc9b1ff
adrabowicz <bambus.ca@gmail.com>
2017-08-19 10:26:48 -0700

    added HttpClient

---
+++ Shared/SharedHttpClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using IdentityModel.Client;
+using Configuration;
+
+namespace Shared
+{
+    public static class SharedHttpClient
+    {
+        public static HttpClient GetHttpClient(string clientName, string clientSecret, string scope)
+        {
+            var accessToken = GetAccessToken(clientName, clientSecret, scope);
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+            return client;
+        }
+
+        public static string GetAccessToken(string clientName, string clientSecret, string scope)
+        {
+            var tokenClient = new TokenClient(
+                Config.IdentityServerTokenIP,
+                clientName,
+                clientSecret);
+
+            var response = tokenClient.RequestClientCredentialsAsync(scope).Result;
+
+            return response.AccessToken;
+        }
+    }
+}
